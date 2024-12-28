03d7d3c85368cdff84dc0e82b0efc85391c8355d
Mujammil-Shaikh1 <mujammilshaikh4091@gmail.com>
2024-12-29 00:07:08 +0530

    feat: Implement header and floating dock components (#2)

---
+++ src/components/header/header.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { HoverBorderGradient } from "../ui/hover-border-gradient";
+import { TextRevealCard } from "../ui/text-reveal-card";
+import { TypewriterEffectSmooth } from "../ui/typewriter-effect";
+export function Header() {
+  const words = [
+    {
+      text: "I",
+    },
+    {
+      text: "am",
+    },
+    {
+      text: "a",
+    },
+    {
+      text: "Frontend",
+    },
+    {
+      text: "Developer",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col p-10 items-center justify-center h-[auto]  ">
+      <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
+        Hello I'm
+      </p>
+      <TextRevealCard
+        text="Mujammil Shaikh"
+        revealText="Mujammil Shaikh"
+        className="p-0 m-0 w-full flex bg-transparent justify-center  border-none h-25"
+      ></TextRevealCard>
+      <TypewriterEffectSmooth words={words} className=" mb- mt-0" />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 gap-4">
+        <div className=" flex justify-center text-center">
+          <HoverBorderGradient
+            containerClassName="rounded-lg border-1 border-transparent bg-gradient-to-r from-cyan-400 to-violet-600 bg-clip-border w-[180px] h-[50px] "
+            as="button"
+            className="dark:bg-black w-[178px] h-[48px] justify-center bg-white text-black dark:text-white flex items-center space-x-2"
+          >
+            <span>Download CV</span>
+          </HoverBorderGradient>
+        </div>
+
+        <HoverBorderGradient
+          containerClassName="rounded-lg border-1 border-transparent bg-gradient-to-r from-cyan-400 to-violet-600 bg-clip-border w-[130px] h-[50px]"
+          as="button"
+          className="dark:bg-black bg-white text-black  dark:text-white flex items-center justify-center space-x-2 w-[128px] h-[48px]"
+        >
+          <span>Let's Talk</span>
+        </HoverBorderGradient>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/home/home.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../header/header";
+import { Navbar } from "../navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
